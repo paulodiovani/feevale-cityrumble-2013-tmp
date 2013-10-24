0518d44ff4bf4ea2cb18f3b5caf81dfe969adbf8
cladimirlima <cladimirlima@gmail>
2013-10-23 17:15:19 -0700

    alteracoes TaMain
    adicionado dois botoes para chamado da page TaLocal

--- worklight/TenhoAcesso/TMP/apps/TenhoAcesso/android/native/assets/www/default/wlclient/js/checksum.js
+++ worklight/TenhoAcesso/TMP/apps/TenhoAcesso/android/native/assets/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":96551116,"date":1382571986325,"machine":"clas-MacBook-Pro.local"};
-/* Date: Wed Oct 23 16:46:26 PDT 2013 */
+var WL_CHECKSUM = {"checksum":727183975,"date":1382573603404,"machine":"clas-MacBook-Pro.local"};
+/* Date: Wed Oct 23 17:13:23 PDT 2013 */
--- worklight/TenhoAcesso/TMP/apps/TenhoAcesso/iphone/native/www/default/wlclient/js/checksum.js
+++ worklight/TenhoAcesso/TMP/apps/TenhoAcesso/iphone/native/www/default/wlclient/js/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":3014016765,"date":1382571981655,"machine":"clas-MacBook-Pro.local"};
-/* Date: Wed Oct 23 16:46:21 PDT 2013 */
+var WL_CHECKSUM = {"checksum":2663032828,"date":1382573599439,"machine":"clas-MacBook-Pro.local"};
+/* Date: Wed Oct 23 17:13:19 PDT 2013 */
