bf88b66989deaaa188131af83f97f8221384426f
Paulo Diovani (paulodiovani) <paulo@diovani.com>
2013-10-28 21:52:12 -0200

    Removidos diversos arquivos gerados no build;

--- worklight/TenhoAcesso/TMP/apps/TenhoAcesso/android/native/assets/www/default/wlclient/js/checksum.js
+++
@@ -1,2 +0,0 @@
-var WL_CHECKSUM = {"checksum":3208736469,"date":1382645215889,"machine":"ADM_INF_002391"};
-/* Date: Thu Oct 24 18:06:55 BRST 2013 */
--- worklight/TenhoAcesso/TMP/apps/TenhoAcesso/iphone/native/www/default/wlclient/js/checksum.js
+++
@@ -1,2 +0,0 @@
-var WL_CHECKSUM = {"checksum":2957078088,"date":1382645220366,"machine":"ADM_INF_002391"};
-/* Date: Thu Oct 24 18:07:00 BRST 2013 */
